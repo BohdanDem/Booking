c344eb142e49051884365c064c0a237413e55268
BohdanDem <bogdandemchuk.1@gmail.com>
2025-10-21 05:13:34 +0000

    payments

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,5 +1,11 @@
-import {IsDate, IsNotEmpty, IsString} from "class-validator";
-import {Type} from "class-transformer";
+import { Type } from 'class-transformer';
+import {
+  IsDate,
+  IsDefined,
+  IsNotEmptyObject,
+  ValidateNested,
+} from 'class-validator';
+import { CreateChargeDto } from '@app/common';
 
 export class CreateReservationDto {
   @IsDate()
@@ -10,11 +16,9 @@ export class CreateReservationDto {
   @Type(() => Date)
   endDate: Date;
   
-  @IsString()
-  @IsNotEmpty()
-  placeId: string;
-  
-  @IsString()
-  @IsNotEmpty()
-  invoiceId: string;
+  @IsDefined()
+  @IsNotEmptyObject()
+  @ValidateNested()
+  @Type(() => CreateChargeDto)
+  charge: CreateChargeDto;
 }
